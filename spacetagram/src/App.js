da3311ed45bcf7448f4d51de8b96a0f913b0fd4b
John Michael De Guzman <jmxdg1029@gmail.com>
2022-01-12 17:59:42 -0500

    space background
    space

--- spacetagram/src/App.js
+++ spacetagram/src/App.js
@@ -6,8 +6,10 @@ import Apod from './Apod.js';
 function App() {
   return (
     <div className="App">
-      <h1>Spacetagram</h1>
-      <p>Brought to you by NASA's image API</p>
+      <div className='header'>
+        <h1>SPACETAGRAM</h1>
+        <p>Brought to you by NASA's image API</p>
+        </div>
       <Apod/>
     </div>
   );
